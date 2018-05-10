52c2f6375613cfd492f718287730fce65882cc0f
1w1ng <921754984@qq.com>
2018-05-10 15:24:18 +0800

    OW

---
+++ projects/OW/js/main.js
@@ -0,0 +1,16 @@
+require.config({
+  paths: {
+    "jquery": './jquery-3.3.1.min',
+    "gotop": './gotop',
+    "carousel": './carousel',
+    "loadmore": './loadmore'
+  }
+});
+
+
+require(['jquery', 'gotop', 'carousel', 'loadmore'], function ($, GoTop, Carousel, loadMore) {
+  Carousel.init($('.carousel '))
+  new GoTop($('.gotop-ct'))
+  loadMore.init($('.wrap'))
+
+});
